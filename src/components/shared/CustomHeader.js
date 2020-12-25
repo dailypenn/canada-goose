1837a5acda688510ffafa7b60b30fc03a02a880d
Justin <justinxlieb@gmail.com>
2020-12-25 13:06:26 -0800

    smoothed shadow shade for header

--- src/components/shared/CustomHeader.js
+++ src/components/shared/CustomHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import {
   View,
   StyleSheet,
@@ -7,83 +7,84 @@ import {
   SafeAreaView,
   Platform,
   Dimensions,
-} from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { PublicationEnum } from "../../../NavigationController";
-import { PublicationPrimaryColorRgba } from "../../utils/branding";
-const DP_LOGO = require("../../static/logos/dp-logo-large.png");
+} from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { PublicationEnum } from '../../../NavigationController'
+import { PublicationPrimaryColorRgba } from '../../utils/branding'
+const DP_LOGO = require('../../static/logos/dp-logo-large.png')
 // const STREET_LOGO = require("../../static/logos/street-logo-large.png");
 // const UTB_LOGO = require("../../static/logos/utb-logo-large.png");
 
 // half the height of the header
-const headerHalf = Math.round(Dimensions.get("window").height) * 0.06;
+const HEADER_HALF = Math.round(Dimensions.get('window').height) * 0.06
 
 const styles = StyleSheet.create({
   barContent: {
-    alignItems: "center",
-    alignSelf: "center",
-    justifyContent: "center",
-    position: "absolute",
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
   },
 
   gradient: {
-    position: "absolute",
+    position: 'absolute',
     left: 0,
     right: 0,
     bottom: 0,
-    height: "100%",
+    height: '100%',
   },
 
   image: {
-    height: "60%",
-    resizeMode: "contain",
-    alignSelf: "center",
+    height: '50%',
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    justifyContent: 'center',
   },
 
   view: {
-    width: "100%",
-    justifyContent: "center",
-    position: "absolute",
+    width: '100%',
+    justifyContent: 'center',
+    position: 'absolute',
     top: 0,
   },
 
   safeAreaView: {
     flex: 1,
-    alignContent: "center",
-    justifyContent: "center",
-    height: headerHalf,
+    alignContent: 'center',
+    justifyContent: 'center',
+    height: HEADER_HALF,
     shadowOffset: {},
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowRadius: 3,
     elevation: 2,
   },
-});
+})
 
 export const CustomHeader = ({ publicationState, contentOffset }) => {
-  const fadeStart = 100;
-  const fadeEnd = 175;
-  const fadeDist = fadeEnd - fadeStart;
-  const dpRed = "rgba(166, 30, 33)";
-  const utbBlue = "rgba(33, 60, 220)";
+  const fadeStart = 100
+  const fadeEnd = 175
+  const fadeDist = fadeEnd - fadeStart
+  const dpRed = 'rgba(166, 30, 33)'
+  const utbBlue = 'rgba(33, 60, 220)'
 
-  function addOpacity(rgbString, opacity) {
-    return rgbString.split(")")[0] + "," + opacity + ")";
+  const addOpacity = (rgbString, opacity) => {
+    return rgbString.split(')')[0] + ',' + opacity + ')'
   }
 
-  function getLogo(publication) {
+  const getLogo = (publication) => {
     // TODO: Implement when we get all logos
-    return DP_LOGO;
+    return DP_LOGO
   }
 
-  function animateMenu() {
+  const animateMenu = () => {
     // TO DO: create animation here
 
     // TO DO: move this to where a publication is selected
     const newPub =
       publicationState.currPublication == PublicationEnum.utb
         ? PublicationEnum.dp
-        : PublicationEnum.utb;
-    publicationState.switchPublication(newPub);
+        : PublicationEnum.utb
+    publicationState.switchPublication(newPub)
   }
 
   return (
@@ -95,22 +96,21 @@ export const CustomHeader = ({ publicationState, contentOffset }) => {
             PublicationPrimaryColorRgba(publicationState.currPublication),
             (contentOffset - fadeStart) / fadeDist
           ),
-          paddingTop: Platform.OS == "ios" ? headerHalf : StatusBar.height,
-          shadowColor: contentOffset < fadeEnd ? null : "#000",
+          paddingTop: Platform.OS == 'ios' ? HEADER_HALF : StatusBar.height,
+          shadowColor: contentOffset < fadeEnd ? null : '#000',
           shadowOffset: contentOffset < fadeEnd ? null : { height: 5 },
           shadowOpacity: contentOffset < fadeEnd ? null : 0.5,
           shadowRadius: contentOffset < fadeEnd ? null : 8,
           elevation: contentOffset < fadeEnd ? null : 4,
         },
       }}
-      // imageStyle={{ opacity: (contentOffset - fadeStart) / fadeDist }}
     >
       <TouchableOpacity onPress={animateMenu} opacity={1}>
         <StatusBar barStyle="light-content" />
         <SafeAreaView
           style={{
             ...styles.safeAreaView,
-            ...{ shadowOpacity: contentOffset < fadeEnd ? 1 : 0 },
+            ...{ shadowOpacity: 1 - (contentOffset - fadeStart) / fadeDist },
           }}
         >
           <Image
@@ -120,5 +120,5 @@ export const CustomHeader = ({ publicationState, contentOffset }) => {
         </SafeAreaView>
       </TouchableOpacity>
     </View>
-  );
-};
+  )
+}
--- src/components/shared/Tagline.js
+++ src/components/shared/Tagline.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
-import { PublicationPrimaryColor } from "../../utils/branding";
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import { PublicationPrimaryColor } from '../../utils/branding'
 
 const styles = StyleSheet.create({
   tagline: {
-    color: "#000",
+    color: '#000',
     fontSize: 14,
-    fontFamily: "HelveticaNeue-CondensedBold",
+    fontFamily: 'HelveticaNeue-CondensedBold',
     flexShrink: 1,
     opacity: 0.5,
   },
@@ -18,21 +18,21 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   view: {
-    flexDirection: "row",
+    flexDirection: 'row',
     padding: 20,
   },
-});
+})
 
 const circleStyle = (publication) => {
   return {
     ...{ color: PublicationPrimaryColor(publication) },
     ...styles.circle,
-  };
-};
+  }
+}
 
 export const Tagline = ({ tagline, publication }) => (
   <View style={styles.view}>
     <View style={circleStyle(publication)} />
     <Text style={styles.tagline}>{tagline}</Text>
   </View>
-);
+)
