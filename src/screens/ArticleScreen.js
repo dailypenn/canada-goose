202ae3b2618aafb210015a6a57ee3419152d0472
Justin <justinxlieb@gmail.com>
2021-01-03 15:53:15 -0800

    displaying hardcoded authors

--- src/screens/ArticleScreen.js
+++ src/screens/ArticleScreen.js
@@ -1,17 +1,33 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, useWindowDimensions } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { PictureHeadline } from '../components/shared'
 import { IMAGE_URL } from '../utils/helperFunctions'
 import HTML from 'react-native-render-html'
 
+// Converts array of authors to displayable format
+const authorsString = (authorArr) => {
+  if (authorArr.length == 0) return 'N/A'
+  const lastAuth = authorArr[authorArr.length - 1]
+  if (authorArr.length == 1) return lastAuth
+
+  return (
+    authorArr.slice(0, -1).join(', ') +
+    `${authorArr.length == 2 ? '' : ','} and ${lastAuth}`
+  )
+}
+
 export const ArticleScreen = ({ navigation, route }) => {
-  console.log(navigation)
-  console.log(route)
-
   const article = route.params.article
-  // TODO: Check that article is already fetched
+  if (!article) {
+    // TODO: Check that article is already fetched
+  }
 
+  /* Currently author and image credits are not supported by
+  GraphQL queries, so hard coding right now */
+  // TODO: Fetch from CEO
+  const authors = ['Ima Ryder', 'Nutha Ryder']
+  const photographer = 'Pitt Shure'
   return (
     <ScrollView>
       <PictureHeadline
@@ -23,11 +39,41 @@ export const ArticleScreen = ({ navigation, route }) => {
         )}
         category="NEWS"
       />
+      <View
+        style={{
+          paddingHorizontal: 20,
+          paddingVertical: 10,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: 'HelveticaNeue-CondensedBold',
+            fontSize: 16,
+          }}
+        >{`By: ${authorsString(authors)}`}</Text>
+        <Text
+          style={{
+            fontFamily: 'HelveticaNeue-CondensedBold',
+            fontSize: 16,
+          }}
+        >
+          {'Photo Credit: ' + photographer}
+        </Text>
+      </View>
       <View style={{ padding: 20 }}>
         <HTML
           source={{ html: article.article.content }}
-          contentWidth={10}
-          tagsStyles={{ p: { fontSize: 16, paddingVertical: 5 } }}
+          contentWidth={useWindowDimensions().width}
+          tagsStyles={{
+            p: {
+              fontSize: 16,
+              lineHeight: 22,
+              paddingBottom: 30,
+              fontFamily: 'HelveticaNeue',
+            },
+            a: { fontSize: 16 },
+            img: { paddingBottom: 10 },
+          }}
         />
       </View>
     </ScrollView>
