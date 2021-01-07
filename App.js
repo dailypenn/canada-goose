5ca3b7b06ddce06d43758dc39e8fd11e6d0e7741
peterbaile <peterchenwflms@gmail.com>
2021-01-07 12:44:57 -0500

    add real graphql server

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { TabNavigationController } from './NavigationController'
 
 // Initialize Apollo Client
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'https://graphql-295919.ue.r.appspot.com/graphql',
   cache: new InMemoryCache(),
 })
 
