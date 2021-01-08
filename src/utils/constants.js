7da58588e75b3e21327f4b7bc7beeb863b82174a
peterbaile <peterchenwflms@gmail.com>
2021-01-08 08:57:44 -0500

    inotherxx show 3 articles instead of 5

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -44,7 +44,70 @@ export const HOME_PAGE_QUERY = gql`
       }
     }
 
-    most_recent: articles(first: 5, section: "news") {
+    inOtherNews: articles(first: 3, section: "app-front-news") {
+      hasNextPage
+      edges {
+        article {
+          slug
+          headline
+          abstract
+          content
+          published_at
+          authors {
+            name
+          }
+          dominantMedia {
+            attachment_uuid
+            extension
+          }
+        }
+        cursor
+      }
+    }
+
+    inOtherOpinion: articles(first: 3, section: "news") {
+      hasNextPage
+      edges {
+        article {
+          slug
+          headline
+          abstract
+          content
+          published_at
+          authors {
+            name
+          }
+          dominantMedia {
+            attachment_uuid
+            extension
+          }
+        }
+        cursor
+      }
+    }
+
+    inOtherSports: articles(first: 3, section: "news") {
+      hasNextPage
+      edges {
+        article {
+          slug
+          headline
+          abstract
+          content
+          published_at
+          authors {
+            name
+          }
+          dominantMedia {
+            attachment_uuid
+            extension
+          }
+        }
+        cursor
+      }
+    }
+
+    inOtherMultimedia: articles(first: 3, section: "news") {
       hasNextPage
       edges {
         article {
@@ -106,48 +169,48 @@ export const SECTIONS = [
     name: 'Academics',
     slug: 'academics',
     image:
-      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg'
   },
   {
     name: 'Administration',
     slug: 'administration',
     image:
-      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg'
   },
   {
     name: 'Identities',
     slug: 'identities',
     image:
-      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg'
   },
   {
     name: 'Politics',
     slug: 'politics',
     image:
-      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg'
   },
   {
     name: 'Student Life',
     slug: 'student-life',
     image:
-      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg'
   },
   {
     name: 'Sports',
     slug: 'sports',
     image:
-      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg'
   },
   {
     name: 'Staff Editorials',
     slug: 'editorials',
     image:
-      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg'
   },
   {
     name: 'Opinion Columns',
     slug: 'columns',
     image:
-      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg',
-  },
+      'https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B800%2C500%5D&w=1600&h=838&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2017%2F05%2Fblue0517.jpg'
+  }
 ]
