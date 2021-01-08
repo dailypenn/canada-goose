22d9d6c6ab3d3510f7a204d3d89dec715d4f2242
Justin <justinxlieb@gmail.com>
2021-01-07 16:28:28 -0800

    added fonts correctly

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Text } from 'react-native'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { TabNavigationController } from './NavigationController'
+
+import * as Font from 'expo-font'
+
+const FONT_FOLDER = './src/static/fonts'
 
 // Initialize Apollo Client
 const client = new ApolloClient({
@@ -8,10 +13,35 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <TabNavigationController />
-  </ApolloProvider>
-)
+export default class App extends React.Component {
+  state = {
+    assetsLoaded: false,
+  }
 
-export default App
+  async componentDidMount() {
+    await Font.loadAsync({
+      Libertinus: require(`${FONT_FOLDER}/Libertinus/LibertinusSerif-Regular.otf`),
+      LibertinusItalic: require(`${FONT_FOLDER}/Libertinus/LibertinusSerif-Italic.otf`),
+      PlayfairDisplayBold: require(`${FONT_FOLDER}/PlayfairDisplay/PlayfairDisplay-Bold.ttf`),
+      PlayfairDisplayMedium: require(`${FONT_FOLDER}/PlayfairDisplay/PlayfairDisplay-Medium.ttf`),
+      PoppinsBold: require(`${FONT_FOLDER}/Poppins/Poppins-Bold.ttf`),
+      PoppinsRegular: require(`${FONT_FOLDER}/Poppins/Poppins-Regular.ttf`),
+    })
+
+    this.setState({ assetsLoaded: true })
+  }
+
+  render() {
+    const { assetsLoaded } = this.state
+
+    if (assetsLoaded) {
+      return (
+        <ApolloProvider client={client}>
+          <TabNavigationController />
+        </ApolloProvider>
+      )
+    } else {
+      return <Text>Loading Assets!</Text>
+    }
+  }
+}
