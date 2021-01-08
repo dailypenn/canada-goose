edf3cafa97c01a33221121099c63cc30d195d0dd
Elizabeth Powell <epow@seas.upenn.edu>
2021-01-07 23:07:19 -0500

    about screen with dummy profile pictures + scroll fix

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,5 +1,18 @@
 import React from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { FlatGrid } from "react-native-super-grid";
+
+const { width, height } = Dimensions.get("window");
+
+const profilePicSize = width / 5;
+const profilePicCellSize = width / 4;
 
 const teamIntro =
   "Hi, we're the tech department at the DP: a team of student software engineers!";
@@ -8,22 +21,19 @@ const mission =
 
 const GOOSE_LOGO = require("../static/logos/goose_from_canada_logo.png");
 
-// StyleSheet;
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: "center",
-    flexDirection: "column",
     backgroundColor: "#eee",
   },
 
-  image: {
+  logo: {
     width: 300,
     height: 300,
   },
 
   sectionContainer: {
-    flex: 1,
+    flexGrow: 1,
     flexDirection: "row",
   },
 
@@ -42,17 +52,60 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "bold",
     marginTop: 20,
-    paddingVertical: 10,
+    paddingVertical: 20,
+  },
+
+  profilePicImage: {
+    paddingHorizontal: 20,
+    width: profilePicSize,
+    height: profilePicSize,
+    borderRadius: profilePicSize / 2,
+  },
+
+  profilePicCell: {
+    alignItems: "center",
+  },
+
+  profileName: {
+    fontSize: 12,
+    color: "#808080",
   },
 });
 
 const people = [
-  { name: "Peter Baile Chen", image: "" },
-  { name: "Elizabeth Powell", image: "" },
-  { name: "Justin Lieb", image: "" },
-  { name: "Raunaq Singh", image: "" },
-  { name: "Daniel Tao", image: "" },
+  {
+    name: "Peter Baile Chen",
+    pic: "../static/logos/default_profile_pic.png",
+  },
+  {
+    name: "Elizabeth Powell",
+    pic: "../static/logos/default_profile_pic.png",
+  },
+  {
+    name: "Justin Lieb",
+    pic: "../static/logos/default_profile_pic.png",
+  },
+  {
+    name: "Raunaq Singh",
+    pic: "../static/logos/default_profile_pic.png",
+  },
+  {
+    name: "Daniel Tao",
+    pic: "../static/logos/default_profile_pic.png",
+  },
 ];
+
+const ProfileCell = ({ name, image }) => {
+  return (
+    <View style={styles.profilePicCell}>
+      <Image
+        source={require("../static/logos/default_profile_pic.png")}
+        style={styles.profilePicImage}
+      />
+      <Text style={styles.profileName}>{name}</Text>
+    </View>
+  );
+};
 
 export const AboutScreen = () => {
   return (
@@ -60,18 +113,17 @@ export const AboutScreen = () => {
       showsVerticalScrollIndicator={false}
       contentContainerStyle={styles.container}
     >
-      <Image source={GOOSE_LOGO} style={styles.image} />
+      <Image source={GOOSE_LOGO} style={styles.logo} />
       <Text style={styles.intro}>{teamIntro}</Text>
       <Text style={styles.mission}>{mission}</Text>
       <Text style={styles.meetTeam}>Meet the Team</Text>
-      {people.map((l, i) => (
-        <View>
-          {/* <Image
-              source={require("../static/logos/default_profile_pic.png")}
-            /> */}
-          <Text>{l.name}</Text>
-        </View>
-      ))}
+      <FlatGrid
+        itemDimension={profilePicCellSize}
+        data={people}
+        renderItem={({ item }) => (
+          <ProfileCell name={item.name} image={item.pic} />
+        )}
+      ></FlatGrid>
     </ScrollView>
   );
 };
