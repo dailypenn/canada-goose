6a038cf80490978c9f6c844bebf09ca18f6f8e79
Justin <justinxlieb@gmail.com>
2021-01-07 16:30:36 -0800

    added font constants

---
+++ src/utils/fonts.js
@@ -0,0 +1,9 @@
+// Title of Category of Article
+export const DISPLAY_SERIF_BOLD = 'PlayfairDisplayBold'
+export const DISPLAY_SERIF_MEDIUM = 'PlayfairDisplayMedium'
+
+export const GEOMETRIC_BOLD = 'PoppinsBold'
+export const GEOMETRIC_REGULAR = 'PoppinsRegular'
+
+export const BODY_SERIF = 'Libertinus'
+export const BODY_SERIF_ITALIC = 'LibertinusItalic'
