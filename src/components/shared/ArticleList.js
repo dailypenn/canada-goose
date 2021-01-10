10b146e7066d320792c10980ea4a5e78a12075a1
peterbaile <peterchenwflms@gmail.com>
2021-01-09 23:06:55 -0500

    fix search -> article bug

--- src/components/shared/ArticleList.js
+++ src/components/shared/ArticleList.js
@@ -11,8 +11,8 @@ export const ArticleList = ({ articles, navigateToArticleScreen }) => (
           headline,
           published_at,
           dominantMedia: { attachment_uuid, extension },
-          authors,
-        },
+          authors
+        }
       } = el
 
       return (
@@ -33,42 +33,35 @@ export const ArticleList = ({ articles, navigateToArticleScreen }) => (
   </View>
 )
 
-export const SearchArticleList = ({
-  articles,
-  navigateToArticleScreen,
-  publication,
-}) => {
-  return (
-    <View style={{ paddingLeft: 0 }}>
-      {articles.map(el => {
-        const {
-          headline,
-          published_at,
-          dominantMedia: { attachment_uuid, extension },
-          authors,
-        } = el
-        return (
-          <TouchableOpacity
-            activeOpacity={1}
-            onPress={() => navigateToArticleScreen(el)}
-            key={headline}
-          >
-            <HorizontalArticleCell
-              style={{
-                borderWidth: 4,
-                borderColor: '#0F0',
-                marginVertical: 10,
-              }}
-              title={headline}
-              category="Politics"
-              imageURL={IMAGE_URL(attachment_uuid, extension)}
-              timeAgo={TIME_AGO(published_at)}
-              authors={AUTHORS(authors)}
-              publication={publication}
-            />
-          </TouchableOpacity>
-        )
-      })}
-    </View>
-  )
-}
+export const SearchArticleList = ({ articles, navigateToArticleScreen }) => (
+  <View style={{ paddingLeft: 0 }}>
+    {articles.map(el => {
+      const {
+        headline,
+        published_at,
+        dominantMedia: { attachment_uuid, extension },
+        authors
+      } = el
+      return (
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={() => navigateToArticleScreen(el)}
+          key={headline}
+        >
+          <HorizontalArticleCell
+            style={{
+              borderWidth: 4,
+              borderColor: '#0F0',
+              marginVertical: 10
+            }}
+            title={headline}
+            category="Politics"
+            imageURL={IMAGE_URL(attachment_uuid, extension)}
+            timeAgo={TIME_AGO(published_at)}
+            authors={AUTHORS(authors)}
+          />
+        </TouchableOpacity>
+      )
+    })}
+  </View>
+)
