da953f60db8e4abfaaa30cf108ae5f3b2af0331e
Justin <justinxlieb@gmail.com>
2021-01-13 11:13:42 -0800

    merge navigation controller

--- NavigationController.js
+++ NavigationController.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useRef } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Ionicons from 'react-native-vector-icons/Ionicons'
@@ -6,39 +6,34 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 import { HomeStack, DiscoveryStack, SettingsStack } from './src/stacks'
 import { PublicationEnum } from './src/utils/constants'
 import { ScreenWithDefaultParams } from './src/screens'
+import { TouchableHighlight } from 'react-native'
 
 const Tab = createBottomTabNavigator()
 
-export class TabNavigationController extends Component {
-  constructor(props) {
-    super(props)
-    this.switchPublication = this.switchPublication.bind(this)
-    this.state = {
-      currPublication: PublicationEnum.dp,
-      switchPublication: this.switchPublication,
+export const TabNavigationController = ({ navigation }) => {
+  const [currPublication, updateCurrPublication] = useRef(PublicationEnum.dp)
+  const [pubMenuVisible, updatePubMenuVisibility] = useRef(false)
+
+  const switchPublication = newPublication => {
+    if (nwePublication != currPublication) {
+      updateCurrPublication(currPublication)
     }
   }
 
-  // Updates state to match for new publication
-  switchPublication(newPublication) {
-    if (newPublication != this.state.currPublication) {
-      this.setState(prevState => ({
-        ...prevState.switchPublication,
-        currPublication: newPublication,
-      }))
-    }
-  }
+  const CHILD_STATE = { currPublication, switchPublication }
 
-  render() {
-    return (
+  return (
+    <>
       <NavigationContainer>
         <Tab.Navigator
           screenOptions={({ route }) => ({
             tabBarIcon: ({ color, size }) => {
               let iconName
 
-              if (route.name === 'HomeStack') iconName = 'ios-home'
-              else if (route.name == 'DiscoveryStack') iconName = 'ios-search'
+              if (route.name === 'HomeStack') {
+                iconName = 'ios-home'
+                return <Ionicons name={iconName} size={size} color={color} />
+              } else if (route.name == 'DiscoveryStack') iconName = 'ios-search'
               else if (route.name === 'SettingsStack') iconName = 'ios-settings'
 
               return <Ionicons name={iconName} size={size} color={color} />
@@ -53,23 +48,100 @@ export class TabNavigationController extends Component {
           <Tab.Screen
             name="HomeStack"
             component={ScreenWithDefaultParams(HomeStack, {
-              state: this.state,
+              state: CHILD_STATE,
             })}
           />
           <Tab.Screen
             name="DiscoveryStack"
             component={ScreenWithDefaultParams(DiscoveryStack, {
-              state: this.state,
+              state: CHILD_STATE,
             })}
           />
           <Tab.Screen
             name="SettingsStack"
             component={ScreenWithDefaultParams(SettingsStack, {
-              state: this.state,
+              state: CHILD_STATE,
             })}
           />
         </Tab.Navigator>
       </NavigationContainer>
-    )
-  }
+    </>
+  )
 }
+
+// export class TabNavigationController extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.switchPublication = this.switchPublication.bind(this)
+//     // this.updatePublicationMenuVisible = this.updatePublicationMenuVisible.bind(
+//     //   this
+//     // )
+
+//     this.state = {
+//       // TODO: Set Publication Enum to what's stored on disk
+//       currPublication: PublicationEnum.dp,
+//       publicationMenuVisible: false,
+//       switchPublication: this.switchPublication,
+//     }
+//   }
+
+//   // Updates state to match for new publication
+//   switchPublication(newPublication) {
+//     if (newPublication != this.state.currPublication) {
+//       this.setState(prevState => ({
+//         ...prevState.switchPublication,
+//         currPublication: newPublication,
+//       }))
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <NavigationContainer>
+//           <Tab.Navigator
+//             screenOptions={({ route }) => ({
+//               tabBarIcon: ({ color, size }) => {
+//                 let iconName
+
+//                 if (route.name === 'HomeStack') {
+//                   iconName = 'ios-home'
+//                   return <Ionicons name={iconName} size={size} color={color} />
+//                 } else if (route.name == 'DiscoveryStack')
+//                   iconName = 'ios-search'
+//                 else if (route.name === 'SettingsStack')
+//                   iconName = 'ios-settings'
+
+//                 return <Ionicons name={iconName} size={size} color={color} />
+//               },
+//             })}
+//             tabBarOptions={{
+//               activeTintColor: '#A61E21',
+//               inactiveTintColor: 'gray',
+//               showLabel: false,
+//             }}
+//           >
+//             <Tab.Screen
+//               name="HomeStack"
+//               component={ScreenWithDefaultParams(HomeStack, {
+//                 state: this.state,
+//               })}
+//             />
+//             <Tab.Screen
+//               name="DiscoveryStack"
+//               component={ScreenWithDefaultParams(DiscoveryStack, {
+//                 state: this.state,
+//               })}
+//             />
+//             <Tab.Screen
+//               name="SettingsStack"
+//               component={ScreenWithDefaultParams(SettingsStack, {
+//                 state: this.state,
+//               })}
+//             />
+//           </Tab.Navigator>
+//         </NavigationContainer>
+//       </>
+//     )
+//   }
+// }
