7049e285e136bc9fb3bd46ef0b6066a6b8c2d6ff
Justin <justinxlieb@gmail.com>
2021-02-14 21:55:02 -0500

    added expand icon

--- src/components/CustomHTML.js
+++ src/components/CustomHTML.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 import { View, TouchableOpacity, Image } from 'react-native'
 import ImageView from 'react-native-image-viewing'
 import HTML from 'react-native-render-html'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 import {
   BODY_SERIF,
   BODY_SERIF_BOLD,
   BODY_SERIF_ITALIC,
   GEOMETRIC_BOLD,
+  GEOMETRIC_REGULAR,
 } from '../utils/fonts'
 import { PublicationPrimaryColor } from '../utils/branding'
+import { Text } from 'react-native'
 
 export const CustomHTML = ({ article, currPublication }) => {
   const [modalVisible, isModalVisible] = useState(false)
@@ -65,30 +68,47 @@ export const CustomHTML = ({ article, currPublication }) => {
 
   const IMGRenderer = (htmlAttribs, passProps) => {
     return (
-      <TouchableOpacity
-        onPress={() => {
-          updateimgURI(htmlAttribs.src)
-          isModalVisible(true)
-        }}
-        key={passProps.key}
-        style={{
-          width: '100%',
-          minHeight: 100,
-          marginBottom: 15,
-        }}
-        activeOpacity={0.9}
-      >
-        <Image
-          source={{ uri: htmlAttribs.src }}
-          style={{
-            flex: 1,
-            backgroundColor: 'blue',
-            resizeMode: 'center',
-            aspectRatio: htmlAttribs['data-width'] / htmlAttribs['data-height'],
-            borderRadius: 2,
+      <>
+        <TouchableOpacity
+          onPress={() => {
+            updateimgURI(htmlAttribs.src)
+            isModalVisible(true)
           }}
-        />
-      </TouchableOpacity>
+          key={passProps.key}
+          style={{
+            width: '100%',
+            minHeight: 100,
+            marginBottom: 15,
+          }}
+          activeOpacity={0.9}
+        >
+          <Image
+            source={{ uri: htmlAttribs.src }}
+            style={{
+              flex: 1,
+              backgroundColor: '#DDD',
+              resizeMode: 'center',
+              aspectRatio:
+                htmlAttribs['data-width'] / htmlAttribs['data-height'],
+              borderRadius: 2,
+            }}
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              alignContent: 'center',
+              position: 'absolute',
+              bottom: 10,
+              right: 10,
+              padding: 4,
+              borderRadius: 5,
+              backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            }}
+          >
+            <Ionicons name="expand-outline" size={16} color={'#666'} />
+          </View>
+        </TouchableOpacity>
+      </>
     )
   }
 
