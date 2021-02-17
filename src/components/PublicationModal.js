3c46a89b0a76a2d5d608584f3b73f60b9ca909cc
Justin <justinxlieb@gmail.com>
2021-02-16 22:10:12 -0500

    added interactiveHomecomponent to publication modal

--- src/components/PublicationModal.js
+++ src/components/PublicationModal.js
@@ -18,6 +18,7 @@ import { PublicationEnum } from '../utils/constants'
 import { switchPublication, toggleScrollToTop } from '../actions'
 import { StatusBar } from 'react-native'
 import { publicationSwitchAnalytics } from '../utils/analytics'
+import { InteractiveHomeComponent } from './InteractiveHomeComponent'
 
 const SCREEN_DIMENSIONS = Dimensions.get('screen')
 const PUBLICATIONS = [
@@ -286,27 +287,32 @@ const PublicationModalComp = ({
         <View style={{ flex: 1 }}></View>
         <View style={styles.view}>
           <View style={styles.bar} />
-          <TouchableOpacity
-            activeOpacity={1}
-            onPress={() => selectedPublication(currPublication)}
+          <InteractiveHomeComponent
+            touchOpacProps={{
+              activeOpacity: 1,
+              onPress: () => selectedPublication(currPublication),
+            }}
           >
             <PublicationOption publication={currPublication} isCurrent={true} />
-          </TouchableOpacity>
+          </InteractiveHomeComponent>
+
           <View style={styles.line} />
           {PUBLICATIONS.map((el, index) => {
             return el == currPublication ? null : (
-              <TouchableOpacity
-                activeOpacity={0.7}
-                onPress={() => selectedPublication(el)}
-                disabled={currentlySwiping}
-                key={index}
+              <InteractiveHomeComponent
+                touchOpacProps={{
+                  activeOpacity: 1,
+                  onPress: () => selectedPublication(currPublication),
+                  key: index,
+                  disabled: currentlySwiping,
+                }}
               >
                 <PublicationOption
                   publication={el}
                   isCurrent={false}
                   key={index}
                 />
-              </TouchableOpacity>
+              </InteractiveHomeComponent>
             )
           })}
         </View>
